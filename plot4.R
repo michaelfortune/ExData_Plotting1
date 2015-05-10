5d1fe4c864738cd3d8daea41e4f48b45da1b593b
michaelfortune <mike_fortune@lineone.net>
2015-05-10 20:17:07 +0100

    updates
    updates

--- plot4.R
+++ plot4.R
@@ -4,23 +4,22 @@ plot4 <- function() {
   par(mfrow=c(2,2))
   
   ## Global Active power plot
-  plot(SelectedData$timestamp,SelectedData$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+  plot(SelectData$timestamp,SelectData$Global_active_power, type="l", xlab="", ylab="Global Active Power")
   
   ## Voltage by Date and Time plot
-  plot(SelectedData$timestamp,SelectedData$Voltage, type="l", xlab="datetime", ylab="Voltage")
+  plot(SelectData$timestamp,SelectData$Voltage, type="l", xlab="datetime", ylab="Voltage")
   
   ## Energy sub metering plot
   ## Create the plot for the first sub metering
-  plot(SelectedData$timestamp,SelectedData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  plot(SelectData$timestamp,SelectData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
   ## Add data for second sub metering
-  lines(SelectedData$timestamp,SelectedData$Sub_metering_2,col="red")
+  lines(SelectData$timestamp,SelectData$Sub_metering_2,col="red")
   ## Add data for third sub metering
-  lines(SelectedData$timestamp,SelectedData$Sub_metering_3,col="blue")
+  lines(SelectData$timestamp,SelectData$Sub_metering_3,col="blue")
   ## Add the legend to the plot
-  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
-  
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) 
   ## Create the plot for Global reactive Power by Date and Time 
-  plot(SelectedData$timestamp,SelectedData$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+  plot(SelectData$timestamp,SelectData$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
   
   ## Copy the plot as a png file called plot4.png to the Working directory. Set the height and width to 480.
   dev.copy(png, file="plot4.png", width=480, height=480)
